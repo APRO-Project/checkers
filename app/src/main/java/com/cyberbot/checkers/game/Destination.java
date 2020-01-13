aaf03afcc9d78306606975c905b3e03015e76c63
bruteus <brute.us@protonmail.com>
2020-01-13 02:20:46 +0100

    Add Destination class

---
+++ app/src/main/java/com/cyberbot/checkers/game/Destination.java
@@ -0,0 +1,23 @@
+package com.cyberbot.checkers.game;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class Destination {
+    public final GridEntry destinationEntry;
+    public final ArrayList<GridEntry> capturedPieces;
+    public final ArrayList<GridEntry> intermediateSteps;
+
+    Destination(@NotNull CaptureChain captureChain) {
+        destinationEntry = captureChain.getLocationAfterCapture();
+        capturedPieces = captureChain.getCapturedPieces();
+        intermediateSteps = captureChain.getIntermediateSteps();
+    }
+
+    Destination(@NotNull GridEntry destinationEntry) {
+        this.destinationEntry = destinationEntry;
+        capturedPieces = null;
+        intermediateSteps = null;
+    }
+}
