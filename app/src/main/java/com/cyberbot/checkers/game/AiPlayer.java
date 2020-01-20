767560eb93e223e873978b571d980928c2e96608
Bartosz Walusiak <bartosz.walusiak.b@gmail.com>
2020-01-20 20:04:21 +0100

    Add tree builder

--- app/src/main/java/com/cyberbot/checkers/game/AiPlayer.java
+++ app/src/main/java/com/cyberbot/checkers/game/AiPlayer.java
@@ -1,38 +1,55 @@
 package com.cyberbot.checkers.game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class AiPlayer {
     private PlayerNum aiNum;
     private PlayerNum adversaryNum;
-    private Move aiMove;
+    private GridEntry aiMoveSrc;
+    private Destination aiMoveDestination;
+    private Tree gameTree;
+    private int lvl = 3;
 
-    public AiPlayer(PlayerNum aiNum) {
+    public AiPlayer(PlayerNum aiNum, PlayerNum adversaryNum) {
         this.aiNum = aiNum;
+        this.adversaryNum = adversaryNum;
     }
 
-    public Move getAiMove(Grid currentGrid) {
-        Move bestMove = null;
-        int topValue = -1000;
-        ArrayList<Move> possibleMoves = new ArrayList<>();   //TODO
-        for (Move move : possibleMoves) {
-            Grid simulated = Grid.simulateMove(currentGrid, move);
+    private void buildTree(Grid rootGrid, int maxDepth){
+        gameTree = new Tree();
+        Node root = new Node(rootGrid, true, 0);
+        gameTree.setRoot(root);
+        buildTree(root, maxDepth);
+    }
 
-            ArrayList<Move> possibleSecondMoves = new ArrayList<>();   //TODO
-            for (Move move2 : possibleSecondMoves) {
-                Grid simulated2 = Grid.simulateMove(simulated, move2);
-
-                ArrayList<Move> possibleThirdMoves = new ArrayList<>();   //TODO
-                for (Move move3 : possibleThirdMoves) {
-                    Grid simulated3 = Grid.simulateMove(simulated2, move3);
-
-                    if (simulated3.getValue(aiNum, adversaryNum) > topValue) {
-                        topValue = simulated3.getValue(aiNum, adversaryNum);
-                        bestMove = move;
+    private void buildTree(Node parentNode, int maxDepth){
+        if (parentNode.getDepth()<maxDepth) {
+            HashMap<GridEntry, ArrayList<Destination>> possibleMoves;
+            if (parentNode.isAiPlayer()) {
+                possibleMoves = parentNode.getGrid().getMovableEntries(aiNum);
+            } else {
+                possibleMoves = parentNode.getGrid().getMovableEntries(adversaryNum);
+            }
+            for (GridEntry src : possibleMoves.keySet()) {
+                for (Destination dst: Objects.requireNonNull(possibleMoves.get(src))){
+                    Node newNode = new Node(src, dst, Grid.simulateMove(parentNode.getGrid(), src, dst), !parentNode.isAiPlayer(), parentNode.getDepth()+1);
+                    parentNode.addChild(newNode);
+                    if (!newNode.getGrid().win(aiNum) && !newNode.getGrid().loose(aiNum)){
+                        buildTree(newNode,maxDepth);
                     }
                 }
             }
         }
-        return bestMove;
+    }
+
+    public HashMap<GridEntry, Destination> getAiMove(Grid grid){
+        buildTree(grid, lvl);
+
+    }
+
+    private HashMap<GridEntry, Destination> getTheBestMove(){
+
     }
 }
