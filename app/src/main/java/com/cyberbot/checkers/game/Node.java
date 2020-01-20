8b457901be6220de71d890b80eead631cf0b7d13
Bartosz Walusiak <bartosz.walusiak.b@gmail.com>
2020-01-20 20:02:42 +0100

    Add Tree.java, Node.java

---
+++ app/src/main/java/com/cyberbot/checkers/game/Node.java
@@ -0,0 +1,75 @@
+package com.cyberbot.checkers.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private GridEntry src;
+    private Destination dst;
+    private Grid grid;
+    private boolean isAiPlayer;
+    private ArrayList<Node> children;
+    private int depth;
+
+    public GridEntry getSrc() {
+        return src;
+    }
+
+    public void setSrc(GridEntry src) {
+        this.src = src;
+    }
+
+    public Destination getDst() {
+        return dst;
+    }
+
+    public void setDst(Destination dst) {
+        this.dst = dst;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    public boolean isAiPlayer() {
+        return isAiPlayer;
+    }
+
+    public void setAiPlayer(boolean aiPlayer) {
+        isAiPlayer = aiPlayer;
+    }
+
+    public ArrayList<Node> getChildren() {
+        return children;
+    }
+
+    public void addChild(Node node){
+        children.add(node);
+    }
+
+    public Node(Grid grid, boolean isAiPlayer, int depth) {
+        this.grid = grid;
+        this.isAiPlayer = isAiPlayer;
+        this.depth = depth;
+    }
+
+    public Node(GridEntry src, Destination dst, Grid grid, boolean isAiPlayer, int depth) {
+        this.src = src;
+        this.dst = dst;
+        this.grid = grid;
+        this.isAiPlayer = isAiPlayer;
+        this.depth = depth;
+    }
+}
---
+++ app/src/main/java/com/cyberbot/checkers/game/Tree.java
@@ -0,0 +1,15 @@
+package com.cyberbot.checkers.game;
+
+public class Tree {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+
+}
