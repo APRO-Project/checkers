dd57e5e20b3604a1e0929821146bca5b48543c3e
bruteus <brute.us@protonmail.com>
2020-01-24 22:07:17 +0100

    Add even more annotations and quality javadocs

--- app/src/main/java/com/cyberbot/checkers/game/Grid.java
+++ app/src/main/java/com/cyberbot/checkers/game/Grid.java
@@ -10,7 +10,6 @@ import com.cyberbot.checkers.preferences.Preferences;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -35,22 +34,25 @@ public class Grid implements Iterable<GridEntry> {
     }
 
     /**
-     * Default constructor, that creates Grid object
-     * with 10x10 board size and 4 player rows
+     * Default constructor, that creates {@link Grid} object
+     * with 10x10 board and 4 player rows.
      */
     public Grid() {
         this(10, 4);
     }
 
     /**
-     * Constructs Grid object with default Preferences
+     * Constructs {@link Grid} object with default {@link Preferences}.
      *
-     * @param size Size of the board. Board's dimensions will be [size x size]
-     * @param playerRows Number of player rows - must not be greater than the
-     *                   size / 2 - 1
+     * @param size Size of the board. Board's dimensions will be [{@code size} x {@code size}].
+     *             Minimum value is 8
+     * @param playerRows Number of player rows - must not be greater than {@code size / 2 - 1}
      */
     public Grid(int size, int playerRows) {
-        if (size - (playerRows * 2) < 2) {
+        if(size < 8) {
+            throw new IllegalArgumentException("Size of the board should be at least 8");
+        }
+        if(size - (playerRows * 2) < 2) {
             throw new IllegalArgumentException("There should be 2 no-player rows on the board at minimum");
         }
 
@@ -76,10 +78,10 @@ public class Grid implements Iterable<GridEntry> {
     }
 
     /**
-     * Constructs Grid object
+     * Constructs {@link Grid} object.
      *
-     * @param size See Grid(int, int)
-     * @param playerRows See Grid(int, int)
+     * @param size See {@link Grid#Grid(int, int)}
+     * @param playerRows See {@link Grid#Grid(int, int)}
      * @param canMoveBackwards Allow ordinary pieces to move backwards
      * @param canCaptureBackwards Allow ordinary pieces to capture backwards
      * @param flyingKing Allow king to move by multiple steps
@@ -94,10 +96,10 @@ public class Grid implements Iterable<GridEntry> {
     }
 
     /**
-     * Constructs Grid object based on given Preferences
+     * Constructs {@link Grid} object based on given {@link Preferences}.
      *
-     * @param prefs Preferences object. Cannot be null
-     * @return Grid object
+     * @param prefs {@link Preferences} object. Cannot be null
+     * @return {@link Grid} object
      *
      * @see Preferences
      */
@@ -115,13 +117,16 @@ public class Grid implements Iterable<GridEntry> {
     }
 
     /**
-     * Look for specific GridEntry in gridEntries
+     * Look for specific {@link GridEntry} in {@link Grid#gridEntries}.
      *
-     * @param x x-coordinate of searched GridEntry
-     * @param y y-coordinate of searched GridEntry
-     * @return GridEntry object if such exists in gridEntries. Otherwise, a RuntimeException is thrown
-     * @throws IndexOutOfBoundsException When called with x or y greater than size or less than zero
+     * @param x x-coordinate of searched {@link GridEntry}
+     * @param y y-coordinate of searched {@link GridEntry}
+     * @return {@link GridEntry} object if such exists in {@link Grid#gridEntries}. Otherwise, a
+     * {@link RuntimeException} is thrown
+     * @throws IndexOutOfBoundsException When called with {@code x} or {@code y} greater than
+     * {@link Grid#size} or less than zero
      */
+    @NotNull
     public GridEntry getEntryByCoords(int x, int y) throws IndexOutOfBoundsException {
         if (x >= size || y >= size || x < 0 || y < 0) {
             throw new IndexOutOfBoundsException("Coordinates (" + x + ", " + y + ") out of bounds for grid with size " + size);
@@ -135,26 +140,36 @@ public class Grid implements Iterable<GridEntry> {
     }
 
     /**
-     * Sets player as NOPLAYER and pieceType as UNASSIGNED for given GridEntry
+     * Sets {@code player} as {@link PlayerNum#NOPLAYER} and {@code pieceType} as {@link PieceType#UNASSIGNED}
+     * in {@link GridEntry} that matches given coordinates. When there is no such entry,
+     * a {@link RuntimeException} is thrown.
      *
-     * @param entry GridEntry to be "removed"
+     * @param entry {@link GridEntry} to be "removed"
      *
      * @see PlayerNum
      * @see PieceType
      */
     public void removeGridEntry(@NotNull GridEntry entry) {
         int index = gridEntries.indexOf(entry);
-        // TODO: Throw RuntimeException when entry not found
+        if(index == -1) {
+            throw new RuntimeException("Entry (" + entry.getX() + ", " + entry.getY() + ") not found in gridEntries");
+        }
+
         gridEntries.get(index).setPlayer(PlayerNum.NOPLAYER);
         gridEntries.get(index).setPieceType(PieceType.UNASSIGNED);
     }
 
     /**
-     * Get Destination object based on given src and dst entries
+     * Get {@link Destination} object based on given {@code src} and {@code dst} entries.
      *
      * @param src Source entry. Cannot be null
      * @param dst Destination entry. Cannot be null
-     * @return
+     * @return {@link Destination} that matches given {@code dst}, new {@link Destination} with
+     * {@code src} as the destination when {@code src == dst} or null when there is no destinations
+     * for given {@code src} or {@code dst} is not in found in possible destinations
+     *
+     * @see Destination
+     * @see Grid#getMovableEntries(PlayerNum)
      */
     @Nullable
     public Destination getDestination(@NotNull GridEntry src, @NotNull GridEntry dst) {
@@ -171,7 +186,14 @@ public class Grid implements Iterable<GridEntry> {
         return null;
     }
 
-    public boolean destinationAllowed(GridEntry src, GridEntry dst) {
+    /**
+     * Tells if a move is allowed from {@code src} to {@code dst}.
+     *
+     * @param src Source entry. Cannot be null
+     * @param dst Destination entry. Cannot be null
+     * @return {@code true} if move from {@code src} to {@code dst} is allowed, {@code false} otherwise
+     */
+    public boolean destinationAllowed(@NotNull GridEntry src, @NotNull GridEntry dst) {
         if(src == dst) return true;
 
         getMovableEntries(src.getPlayer());
@@ -186,7 +208,21 @@ public class Grid implements Iterable<GridEntry> {
         return false;
     }
 
-    public boolean attemptMove(GridEntry src, GridEntry dst) {
+    /**
+     * Perform a move based on given {@code src} and {@code dst}. If the move is not
+     * allowed based by {@link Grid#destinationAllowed(GridEntry, GridEntry)} decision or when
+     * {@code src == dst}, it isn't executed.
+     *
+     * Moving a piece includes swapping {@code player} and {@code pieceType} of involved entries.
+     *
+     * If any of entries is not found in {@link Grid#gridEntries}, a {@link RuntimeException} is
+     * thrown.
+     *
+     * @param src Source entry. Cannot be null
+     * @param dst Destination entry. Cannot be null
+     * @return {@code true} is move was executed or {@code false} if it's not allowed
+     */
+    public boolean attemptMove(@NotNull GridEntry src, @NotNull GridEntry dst) {
         if(src == dst) return false;
 
         if(!destinationAllowed(src, dst)) return false;
@@ -204,6 +240,8 @@ public class Grid implements Iterable<GridEntry> {
         gridEntries.get(srcIdx).setPlayer(PlayerNum.NOPLAYER);
         gridEntries.get(srcIdx).setPieceType(PieceType.UNASSIGNED);
 
+        // TODO: Promote piece to king if necessary (an update javadoc)
+
         movableEntriesCache = null;
 
         return true;
@@ -215,10 +253,17 @@ public class Grid implements Iterable<GridEntry> {
         return gridEntries.iterator();
     }
 
-    private HashSet<GridEntry> getAdjacentEntries(@NotNull GridEntry entry) {
-        HashSet<GridEntry> adjacentEntries = new HashSet<>();
+    /**
+     * Get all adjacent entries to {@code entry}. Only legal entries are considered adjacent.
+     *
+     * @param entry {@link GridEntry}, for which we want to get adjacent entries
+     * @return {@link ArrayList} with all adjacent entries to given {@code entry}
+     */
+    @NotNull
+    private ArrayList<GridEntry> getAdjacentEntries(@NotNull GridEntry entry) {
+        ArrayList<GridEntry> adjacentEntries = new ArrayList<>();
 
-        // Short name for code readability
+        // Short names for code readability
         final int x = entry.getX();
         final int y = entry.getY();
 
@@ -261,21 +306,46 @@ public class Grid implements Iterable<GridEntry> {
         return adjacentEntries;
     }
 
-    private HashSet<GridEntry> getAllowedMoves(@NotNull GridEntry entry) {
-        HashSet<GridEntry> allowedMoves = new HashSet<>();
+    /**
+     * Get all allowed moves for specified {@code entry}. Moves are calculated differently
+     * based on {@link PieceType}.
+     *
+     * @param entry {@link GridEntry} we want to get moves for
+     * @return {@link ArrayList} of allowed moves
+     *
+     * @see Grid#calculateOrdinaryPieceMoves(GridEntry)
+     */
+    @NotNull
+    private ArrayList<GridEntry> getAllowedMoves(@NotNull GridEntry entry) {
+        ArrayList<GridEntry> allowedMoves = new ArrayList<>();
 
         if(entry.getPieceType() == PieceType.ORDINARY) {
             allowedMoves = calculateOrdinaryPieceMoves(entry);
         }
         else if(entry.getPieceType() == PieceType.KING) {
-            // TODO: Calculate king moves
+            // TODO: Calculate king moves (and update javadoc)
         }
 
         return allowedMoves;
     }
 
-    private HashSet<GridEntry> calculateOrdinaryPieceMoves(@NotNull GridEntry entry) {
-        HashSet<GridEntry> allowedMoves = new HashSet<>();
+    /**
+     * Calculate allowed moves for {@link PieceType#ORDINARY}. {@link Grid#canMoveBackwards}
+     * preference is taken into account during calculations.
+     *
+     * If supplied {@code entry} has different {@code pieceType} than {@link PieceType#ORDINARY},
+     * a {@link IllegalArgumentException} is thrown.
+     *
+     * @param entry {@link GridEntry} we want to calculate moves for
+     * @return {@link ArrayList} of calculated moves
+     */
+    @NotNull
+    private ArrayList<GridEntry> calculateOrdinaryPieceMoves(@NotNull GridEntry entry) {
+        if(entry.getPieceType() != PieceType.ORDINARY) {
+            throw new IllegalArgumentException("Attempt to calculate ordinary piece moves for non-ordinary piece");
+        }
+
+        ArrayList<GridEntry> allowedMoves = new ArrayList<>();
         for (GridEntry adjEntry : getAdjacentEntries(entry)) {
             if (adjEntry.getPlayer() == PlayerNum.NOPLAYER) {
                 if (canMoveBackwards
@@ -289,6 +359,14 @@ public class Grid implements Iterable<GridEntry> {
         return allowedMoves;
     }
 
+    /**
+     * Get all allowed captures for specified {@code entry}. Captures are calculated differently
+     * base on {@link PieceType}
+     *
+     * @param entry {@link GridEntry} we want to get captures for
+     * @return {@link ArrayList} of allowed captures
+     */
+    @NotNull
     private ArrayList<CaptureChain> getAllowedCaptures(@NotNull GridEntry entry) {
         CaptureChain root = new CaptureChain(entry, null, null);
 
@@ -296,7 +374,7 @@ public class Grid implements Iterable<GridEntry> {
             calculateOrdinaryPieceCaptures(root, entry.getPlayer());
         }
         else if(entry.getPieceType() == PieceType.KING) {
-            // TODO: Calculate king captures
+            // TODO: Calculate king captures (and update javadoc)
         }
 
         ArrayList<CaptureChain> allowedCaptures;
@@ -307,6 +385,23 @@ public class Grid implements Iterable<GridEntry> {
         return allowedCaptures;
     }
 
+    /**
+     * Calculate allowed captures for {@link PieceType#ORDINARY}. {@link Grid#canCaptureBackwards}
+     * preference is taken into account during calculations.
+     *
+     * Method doesn't return any value - calculations are performed recursively and results are
+     * stored in {@code lastCapture} parameter.
+     *
+     * There's no checks if the method was invoked for {@link PieceType#ORDINARY} because of
+     * recursive nature of it, so the caller should be sure to call it correctly. Also, the caller
+     * needs to be sure, that {@code player} is not set to {@link PlayerNum#NOPLAYER}.
+     *
+     * @param lastCapture {@link CaptureChain} object representing last capture that has taken place.
+     *                                        It should have been set to root capture when first invoked
+     * @param player {@link PlayerNum} representing the player who is capturing
+     *
+     * @see CaptureChain
+     */
     private void calculateOrdinaryPieceCaptures(@NotNull CaptureChain lastCapture, final PlayerNum player) {
         GridEntry lastLocation = lastCapture.getLocationAfterCapture();
 
@@ -341,7 +436,28 @@ public class Grid implements Iterable<GridEntry> {
         }
     }
 
+    /**
+     * Get all movable entries for given {@code player}. This includes all moves and captures,
+     * respecting the {@link Grid#mandatoryCapture} preference.
+     *
+     * Results are saved in {@link Grid#movableEntriesCache} because of high calling rate
+     * and quite complex operations withing the method. The cache should be cleared after
+     * {@link Grid#attemptMove(GridEntry, GridEntry)} is successfully called ({@code true} returned).
+     *
+     * {@code player} should not be {@link PlayerNum#NOPLAYER} - otherwise {@link IllegalArgumentException}
+     * is thrown.
+     *
+     * @param player Player we want to get movable entries for
+     * @return {@link HashMap}, where {@link GridEntry} is the key and represents source of the move
+     * and {@link ArrayList} of {@link Destination} that represents all possible destinations for the
+     * source entry
+     */
+    @NotNull
     public HashMap<GridEntry, ArrayList<Destination>> getMovableEntries(PlayerNum player) {
+        if(player == PlayerNum.NOPLAYER) {
+            throw new IllegalArgumentException("Cannot get movable entries for NOPLAYER");
+        }
+
         if (movableEntriesCache != null) return movableEntriesCache;
 
         HashMap<GridEntry, ArrayList<Destination>> movableEntries = new HashMap<>();
@@ -367,7 +483,7 @@ public class Grid implements Iterable<GridEntry> {
         if (!mandatoryCapture || possibleCaptures.isEmpty()) {
             for (GridEntry entry : gridEntries) {
                 if (entry.getPlayer() == player) {
-                    HashSet<GridEntry> allowedMoves = getAllowedMoves(entry);
+                    ArrayList<GridEntry> allowedMoves = getAllowedMoves(entry);
                     if (!allowedMoves.isEmpty()) {
                         ArrayList<Destination> destinations = new ArrayList<>();
                         for (GridEntry destination : allowedMoves) {
@@ -394,52 +510,57 @@ public class Grid implements Iterable<GridEntry> {
                     if (!movableEntries.containsKey(possibleCapture.getKey())) {
                         movableEntries.put(possibleCapture.getKey(), destinations);
                     } else {
-                        movableEntries.get(possibleCapture.getKey()).addAll(destinations);
+                        ArrayList<Destination> otherDestinations = movableEntries.get(possibleCapture.getKey());
+                        if(otherDestinations == null) {
+                            throw new RuntimeException("This should not happen. Contact developers on GitHub if it did");
+                        }
+                        otherDestinations.addAll(destinations);
                     }
                 }
             }
         }
+
         movableEntriesCache = movableEntries;
 
         return movableEntries;
     }
 
-      int getValue(){
+    int getValue(){
         int value = 0;
         for (GridEntry gridEntry : gridEntries) {
+            // Count pieces
+            value += 10;
 
-                //count pieces
+            if (gridEntry.getPieceType() == PieceType.KING) {
+                value += 30;
+            }
 
-                value += 10;
+            // Prioritize sides
+            value += Math.abs((gridEntry.getX()) + gridEntry.getY() - getSize()) / 4;
 
-                if (gridEntry.getPieceType() == PieceType.KING) {
-                    value += 30;
-                }
-
-                //prioritize sides
-
-                value += Math.abs((gridEntry.getX()) + gridEntry.getY() - getSize()) / 4;
-
-                //prioritize forward
-
-                if (gridEntry.getPlayer() == PlayerNum.FIRST) {
-                    value += gridEntry.getY() / 2;
-                } else if (gridEntry.getPlayer() == PlayerNum.SECOND) {
-                    value += Math.abs((gridEntry.getY() - getSize()) / 2);
-                }
+            // Prioritize forward
+            if (gridEntry.getPlayer() == PlayerNum.FIRST) {
+                value += gridEntry.getY() / 2;
+            } else if (gridEntry.getPlayer() == PlayerNum.SECOND) {
+                value += Math.abs((gridEntry.getY() - getSize()) / 2);
+            }
         }
+
         return value;
     }
 
     static Grid simulateMove(Grid grid, GridEntry src, Destination dst) {
         grid.getEntryByCoords(src.getX(), src.getY()).setPlayer(PlayerNum.NOPLAYER);
         grid.getEntryByCoords(src.getX(), src.getY()).setPieceType(PieceType.UNASSIGNED);
+
         for (GridEntry destroyed:dst.getCapturedPieces()){
             grid.getEntryByCoords(destroyed.getX(),destroyed.getY()).setPieceType(PieceType.UNASSIGNED);
             grid.getEntryByCoords(destroyed.getX(),destroyed.getY()).setPlayer(PlayerNum.NOPLAYER);
         }
+
         grid.getEntryByCoords(dst.getDestinationEntry().getX(),dst.getDestinationEntry().getY()).setPlayer(PlayerNum.NOPLAYER);
         grid.getEntryByCoords(dst.getDestinationEntry().getX(),dst.getDestinationEntry().getY()).setPieceType(PieceType.UNASSIGNED);
+
         return grid;
     }
 
